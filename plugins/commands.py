37b32ba2bc8850c28297e1d0281d75b0ff94adff
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-09-19 11:21:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,8 +42,9 @@ async def start(client, message):
             InlineKeyboardButton('GROUP', url='https://t.me/Agunivers_backup'), 
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-                START_TXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
